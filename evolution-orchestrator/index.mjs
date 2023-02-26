ae8561118a164055d0769fe58ed292cdac9eba20
Radu Zaharia <101859038+raduzaharia-medium@users.noreply.github.com>
2023-02-26 16:34:12 +0000

    Parallel create child.

--- evolution-orchestrator/index.mjs
+++ evolution-orchestrator/index.mjs
@@ -23,17 +23,20 @@ export default orchestrator(function* (context) {
   });
   nextGeneration.push(best);
 
+  const tasks = [];
   for (const element of population) {
     const other = population[Math.round(Math.random() * (population.length - 1))];
-    const child = yield context.df.callSubOrchestrator("create-child-orchestrator", {
-      parent1: element,
-      parent2: other,
-      mutationProbability: 0.2,
-    });
-
-    if (child) nextGeneration.push(child);
+    tasks.push(
+      context.df.callSubOrchestrator("create-child-orchestrator", {
+        parent1: element,
+        parent2: other,
+        mutationProbability: 0.2,
+      })
+    );
   }
+  const children = yield context.df.Task.all(tasks);
 
+  for (const element of children) if (element) nextGeneration.push(element);
   yield context.df.callEntity(state, "setPopulation", nextGeneration);
 
   const finalBest = yield context.df.callSubOrchestrator("pick-best-vector-orchestrator", { vectors: nextGeneration });
